042dfe210ad2cfe89e7760e803ec48546d70d8e2
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-12-19 15:29:23 +0100

    fixed @waweewawee's design

--- client/src/routes/homepage/Homepage.tsx
+++ client/src/routes/homepage/Homepage.tsx
@@ -1,133 +1,154 @@
 import {
-	Button, CardActions, CardContent, CardMedia, Grid, ThemeProvider 
+	Box,
+	Button, CardActionArea, CardActions, cardClasses, CardContent, CardMedia, Divider, Grid, ThemeProvider
 } from '@mui/material';
 import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
 import Typography from '@mui/material/Typography';
 import {
 	Container
 } from '@mui/system';
 import React from 'react';
 import UserContext from '../../context/UserContext';
+import Monki from '../../assets/gorilla.jfif';
+
+type TCard = {
+		naam: string,
+		datum: string,
+		omschrijving: string,
+		afbeelding:string
+		
+}[]
+
+const cards: TCard = [
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+	{
+		naam: 'niggas in paris',
+		afbeelding: Monki,
+		datum: '69th of your mom',
+		omschrijving:'SUPER GAAF'
+	},
+];
 
 function Homepage() {
-	const { user, theme } = React.useContext(UserContext);
+	const { theme } = React.useContext(UserContext);
 	return (
 		<ThemeProvider theme={theme.theme}>
 			<Container>
-				<Card sx={{
-					maxWidth: 2000
+				<Typography sx={{
+					py: 2
+				}} component={'h1'}
+				textAlign={'center'}
+				variant="h3">Welkom bij theater Baget!</Typography>
+				<Box sx={{
+					display: 'flex',
+					justifyContent: 'center',
 				}}>
-					<Typography textAlign={'center'} variant="h3">Welkom bij theater Baget!</Typography>
-				</Card>
-				<Grid item xs={11}>
-					<iframe width="560" height="315"
-						src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowFullScreen></iframe>
-				</Grid>
-				<Card sx={{
-					maxWidth: 3000
-				}}>
-					<Typography textAlign={'center'} variant="h4">Huidige activiteiten</Typography>
-				</Card>
-				<Grid container spacing={3}>
-					<Grid item xs={3}>
-						<Card sx={{
-							maxWidth: 260 
+					<Card
+						elevation={3}
+						sx={{
+							width: 500,
+							mb: 2,
+							p: 4
 						}}>
-							<CardMedia
-								component="img"
-								height="120"
-								image="/src/components/gorilla.jfif"
-								alt="gorilla"
-							/>
-	  <CardContent>
-								<Typography gutterBottom variant="h5"
-									component="div">
-          Programma1
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-          Omschrijving programma1
-								</Typography>
-								<CardActions>
-									<Button size="small">Bekijk programma</Button>
-								</CardActions>
-							</CardContent>
-						</Card>
-					</Grid>
-					<Grid item xs={3}>
-						<Card sx={{
-							maxWidth: 260 
-						}}>
-							<CardMedia
-								component="img"
-								height="120"
-								image="/src/components/gorilla.jfif"
-								alt="gorilla"
-							/>
-	  <CardContent>
-								<Typography gutterBottom variant="h5"
-									component="div">
-          Programma2
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-          Omschrijving programma2
-								</Typography>
-								<CardActions>
-									<Button size="small">Bekijk programma</Button>
-								</CardActions>
-							</CardContent>
-						</Card>
-					</Grid>
-					<Grid item xs={3}>
-						<Card sx={{
-							maxWidth: 260 
-						}}>
-							<CardMedia
-								component="img"
-								height="120"
-								image="/src/components/gorilla.jfif"
-								alt="gorilla"
-							/>
-	  <CardContent>
-								<Typography gutterBottom variant="h5"
-									component="div">
-          Programma3
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-          Omschrijving programma3
-								</Typography>
-								<CardActions>
-									<Button size="small">Bekijk programma</Button>
-								</CardActions>
-							</CardContent>
-						</Card>
-					</Grid>
-					<Grid item xs={3}>
-						<Card sx={{
-							maxWidth: 260 
-						}}>
-							<CardMedia
-								component="img"
-								height="120"
-								image="/src/components/gorilla.jfif"
-								alt="gorilla"
-							/>
-	  <CardContent>
-								<Typography gutterBottom variant="h5"
-									component="div">
-          Programma4
-								</Typography>
-								<Typography variant="body2" color="text.secondary">
-          Omschrijving programma4
-								</Typography>
-								<CardActions>
-									<Button size="small">Bekijk programma</Button>
-								</CardActions>
-							</CardContent>
-						</Card>
-					</Grid>
+						<CardMedia>
+							<iframe width="500" height="315"
+								src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player"
+								allow="accelerometer; autoplay; clipboard-write;
+							 encrypted-media; gyroscope; picture-in-picture"
+								allowFullScreen>
+
+							</iframe>
+						</CardMedia>
+					</Card>
+				</Box>
+				<Divider sx={{
+					my:2
+				}}/>
+				<Typography textAlign={'center'} variant="h4">Huidige activiteiten</Typography>
+				<Grid container spacing={3}
+					sx={{
+						p: 4
+					}}>
+					{
+						cards.map((card) => {
+							return (
+								<Grid item xs={4}
+									key={card.naam}>
+									<Card elevation={4}>
+										<CardActionArea>
+
+											<CardMedia
+												component="img"
+												height=""
+												image={card.afbeelding}
+												alt="gorilla"
+											/>
+											<CardContent>
+												<Typography gutterBottom variant="h5"
+													component="div">
+													{card.naam}
+												</Typography>
+												<Typography variant="body2" color="text.secondary">
+													{card.datum}
+												</Typography>
+												<Typography variant="body2" color="text.secondary">
+													{card.omschrijving}
+												</Typography>
+											</CardContent>
+										</CardActionArea>
+
+									</Card>
+								</Grid>
+							);
+						})
+					}
 				</Grid>
 			</Container>
 		</ThemeProvider>
