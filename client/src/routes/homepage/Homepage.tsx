04666505dbcce7da854bb6a19fa9673d2291c521
waweewawee <wilmsakasha@gmail.com>
2022-12-19 14:24:17 +0100

    niet belangrijk

--- client/src/routes/homepage/Homepage.tsx
+++ client/src/routes/homepage/Homepage.tsx
@@ -1,5 +1,5 @@
 import {
-	Button, CardActions, CardContent, CardMedia, Grid 
+	Button, CardActions, CardContent, CardMedia, Grid, ThemeProvider 
 } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardActionArea from '@mui/material/CardActionArea';
@@ -8,128 +8,129 @@ import {
 	Container
 } from '@mui/system';
 import React from 'react';
-
-//const { user, theme } = React.useContext(UserContext);
+import UserContext from '../../context/UserContext';
 
 function Homepage() {
+	const { user, theme } = React.useContext(UserContext);
 	return (
-		<Container>
-			<Card sx={{
-				maxWidth: 2000
-			}}>
-				<Typography textAlign={'center'} variant="h3">Welkom bij theater Baget!</Typography>
-			</Card>
-			<Grid item xs={11}>
-				<iframe width="560" height="315"
-					src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowFullScreen></iframe>
-			</Grid>
-			<Card sx={{
-				maxWidth: 3000
-			}}>
-				<Typography textAlign={'center'} variant="h4">Huidige activiteiten</Typography>
-			</Card>
-			<Grid container spacing={3}>
-				<Grid item xs={3}>
-					<Card sx={{
-						maxWidth: 260 
-					}}>
-						<CardMedia
-							component="img"
-							height="120"
-							image="/src/components/gorilla.jfif"
-							alt="gorilla"
-						/>
+		<ThemeProvider theme={theme.theme}>
+			<Container>
+				<Card sx={{
+					maxWidth: 2000
+				}}>
+					<Typography textAlign={'center'} variant="h3">Welkom bij theater Baget!</Typography>
+				</Card>
+				<Grid item xs={11}>
+					<iframe width="560" height="315"
+						src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen></iframe>
+				</Grid>
+				<Card sx={{
+					maxWidth: 3000
+				}}>
+					<Typography textAlign={'center'} variant="h4">Huidige activiteiten</Typography>
+				</Card>
+				<Grid container spacing={3}>
+					<Grid item xs={3}>
+						<Card sx={{
+							maxWidth: 260 
+						}}>
+							<CardMedia
+								component="img"
+								height="120"
+								image="/src/components/gorilla.jfif"
+								alt="gorilla"
+							/>
 	  <CardContent>
-							<Typography gutterBottom variant="h5"
-								component="div">
+								<Typography gutterBottom variant="h5"
+									component="div">
           Programma1
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
           Omschrijving programma1
-							</Typography>
-							<CardActions>
-								<Button size="small">Bekijk programma</Button>
-							</CardActions>
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={3}>
-					<Card sx={{
-						maxWidth: 260 
-					}}>
-						<CardMedia
-							component="img"
-							height="120"
-							image="/src/components/gorilla.jfif"
-							alt="gorilla"
-						/>
+								</Typography>
+								<CardActions>
+									<Button size="small">Bekijk programma</Button>
+								</CardActions>
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={3}>
+						<Card sx={{
+							maxWidth: 260 
+						}}>
+							<CardMedia
+								component="img"
+								height="120"
+								image="/src/components/gorilla.jfif"
+								alt="gorilla"
+							/>
 	  <CardContent>
-							<Typography gutterBottom variant="h5"
-								component="div">
+								<Typography gutterBottom variant="h5"
+									component="div">
           Programma2
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
           Omschrijving programma2
-							</Typography>
-							<CardActions>
-								<Button size="small">Bekijk programma</Button>
-							</CardActions>
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={3}>
-					<Card sx={{
-						maxWidth: 260 
-					}}>
-						<CardMedia
-							component="img"
-							height="120"
-							image="/src/components/gorilla.jfif"
-							alt="gorilla"
-						/>
+								</Typography>
+								<CardActions>
+									<Button size="small">Bekijk programma</Button>
+								</CardActions>
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={3}>
+						<Card sx={{
+							maxWidth: 260 
+						}}>
+							<CardMedia
+								component="img"
+								height="120"
+								image="/src/components/gorilla.jfif"
+								alt="gorilla"
+							/>
 	  <CardContent>
-							<Typography gutterBottom variant="h5"
-								component="div">
+								<Typography gutterBottom variant="h5"
+									component="div">
           Programma3
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
           Omschrijving programma3
-							</Typography>
-							<CardActions>
-								<Button size="small">Bekijk programma</Button>
-							</CardActions>
-						</CardContent>
-					</Card>
-				</Grid>
-				<Grid item xs={3}>
-					<Card sx={{
-						maxWidth: 260 
-					}}>
-						<CardMedia
-							component="img"
-							height="120"
-							image="/src/components/gorilla.jfif"
-							alt="gorilla"
-						/>
+								</Typography>
+								<CardActions>
+									<Button size="small">Bekijk programma</Button>
+								</CardActions>
+							</CardContent>
+						</Card>
+					</Grid>
+					<Grid item xs={3}>
+						<Card sx={{
+							maxWidth: 260 
+						}}>
+							<CardMedia
+								component="img"
+								height="120"
+								image="/src/components/gorilla.jfif"
+								alt="gorilla"
+							/>
 	  <CardContent>
-							<Typography gutterBottom variant="h5"
-								component="div">
+								<Typography gutterBottom variant="h5"
+									component="div">
           Programma4
-							</Typography>
-							<Typography variant="body2" color="text.secondary">
+								</Typography>
+								<Typography variant="body2" color="text.secondary">
           Omschrijving programma4
-							</Typography>
-							<CardActions>
-								<Button size="small">Bekijk programma</Button>
-							</CardActions>
-						</CardContent>
-					</Card>
+								</Typography>
+								<CardActions>
+									<Button size="small">Bekijk programma</Button>
+								</CardActions>
+							</CardContent>
+						</Card>
+					</Grid>
 				</Grid>
-			</Grid>
-		</Container>
-
+			</Container>
+		</ThemeProvider>
 	);
 }
 
