1fce10f641beb75151038a54fa33c4e55e8b7600
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-12-19 13:52:11 +0100

    added the logo and remove d the sidebar bc its not needed. also fixed the colors.

--- client/src/components/global/Topbar.tsx
+++ client/src/components/global/Topbar.tsx
@@ -3,26 +3,24 @@ import React, {
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+
 import {
 	ThemeProvider
 } from '@mui/material/styles';
 import {
-	Button,
-	Tooltip, Typography
+	Button, IconButton, Typography
 } from '@mui/material';
 import {
 	useNavigate
 } from 'react-router-dom';
+import baguette from '../../assets/baguette.png';
 import axios from 'axios';
 import UserContext from '../../context/UserContext';
-import Sidebar from './Sidebar';
 import PersonIcon from '@mui/icons-material/Person';
+import PaidIcon from '@mui/icons-material/Paid';
 
 export default function Topbar() {
 	const navigate = useNavigate();
-	const [isMenuOpen, setMenuOpen] = React.useState(false);
 	const { user, theme } = React.useContext(UserContext);
 	React.useEffect(() => {
 		const fetchData = async () => {
@@ -52,51 +50,50 @@ export default function Topbar() {
 		// do not include user, it will create an infinite loop.
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [navigate]);
-
-	const openSidebarMenu = React.useCallback(async () => {
-		setMenuOpen(true);
-	}, []);
 	return (
 		<ThemeProvider theme={theme.theme}>
-
-			<Sidebar isMenuOpen={isMenuOpen}
-				setMenuOpen={setMenuOpen} />
 			<Box sx={{
 				flexGrow: 1
 			}}>
 				<AppBar position="static" sx={{
-					bgcolor:'secondary.main'
+					bgcolor:'primary.main'
 				}}>
 					<Toolbar sx={{
 						display: 'flex',
 						justifyContent:'space-between',
 					}}>
-						<Box sx={{
-							display: 'flex',
-							alignItems:'center'
-						}}>
-							<Tooltip title="Open Menu">
-								<IconButton
-									size="large"
-									edge="start"
-									color="inherit"
-									aria-label="menu"
-									sx={{
-										mr: 2
-									}}
-									onClick={openSidebarMenu}
-								>
-									<MenuIcon />
-								</IconButton>
-							</Tooltip>
-							<Typography variant='body1'flexGrow={1}>
-                                Theater Baget
-							</Typography>
+						<Box onClick={() => navigate('/')}
+							sx={{
+								display: 'flex',
+								alignItems:'center'
+							}}>
+							<img style={{
+								borderRadius: '50px',
+								width: '35px'
+							}} src={baguette}
+							alt="Theater Baget logo" />
+
+							<Typography variant='body1' sx={{
+								ml: 1,
+								cursor:'pointer'
+							}}
+							flexGrow={1}>
+                                    Theater Baget
+							</Typography>  							
 						</Box>					
 						<Box sx={{
 							display: 'flex',
 						}}>
-							<Button startIcon={<PersonIcon/>} variant='contained'>Login</Button>
+							<Button color='secondary' startIcon={<PaidIcon/>}
+								variant='contained' sx={{
+									mr:2
+								}}>
+                                Donate
+							</Button>
+							<Button color='secondary' startIcon={<PersonIcon/>}
+								variant='contained'>
+                                Login
+							</Button>
 						</Box>
 					</Toolbar>
 				</AppBar>
