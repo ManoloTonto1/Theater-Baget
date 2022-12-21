8fd1a51779eb24ee6d0aa2dbdd45cdc513f62813
waweewawee <wilmsakasha@gmail.com>
2022-12-21 12:06:40 +0100

    LoadingPage fixed

--- client/src/components/global/LoadingPage.tsx
+++ client/src/components/global/LoadingPage.tsx
@@ -1,14 +1,42 @@
 import {
+	Box,
+	Card,
+	CardMedia,
+	CircularProgress,
+	Divider,
+	Grid,
+	ThemeProvider,
 	Typography 
 } from '@mui/material';
+import {
+	Container 
+} from '@mui/system';
 import React from 'react';
 import UserContext from '../../context/UserContext';
 
 function LoadingPage(){
+	const { theme } = React.useContext(UserContext);
 	return(
-		<Typography>
-        Allo
-		</Typography>
+		<ThemeProvider theme={theme.theme}>
+			<Grid
+				container
+				direction="column"
+				alignItems="center"
+				justifyContent="center"
+				sx={{
+					minHeight:'100%',
+					height:'68vh'
+				}}
+			>
+				<CircularProgress />
+				<Typography sx={{
+					py: 3
+				}}
+				textAlign={'center'}>
+                    Even geduld A.U.B..... 
+				</Typography>
+			</Grid>
+		</ThemeProvider>
 	);
 }
 
