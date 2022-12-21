015ffd6691c65e6a984264fd595804bb648bd938
waweewawee <wilmsakasha@gmail.com>
2022-12-21 10:04:30 +0100

    lege loading page toegevoegd

---
+++ client/src/components/global/LoadingPage.tsx
@@ -0,0 +1,13 @@
+import {
+	Typography 
+} from '@mui/material';
+import React from 'react';
+import UserContext from '../../context/UserContext';
+
+function LoadingPage(){
+	<Typography>
+        Allo
+	</Typography>;
+}
+
+export default LoadingPage;
