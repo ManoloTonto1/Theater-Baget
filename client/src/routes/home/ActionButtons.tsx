0f1cae4b82c6d956417681489f015c425dcbcf23
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2022-12-23 10:34:03 +0100

    changes to the home page (#46)
    * changes to the home page
    
    * removed the unused theater.png file from the client/src/assets folder
    
    * fixed issue with the server
    
    * Added a back button and made the topbar responsive.

---
+++ client/src/routes/home/ActionButtons.tsx
@@ -0,0 +1,73 @@
+import {
+	CardActionArea, Grid, Slide 
+} from '@mui/material';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+import LocalActivityIcon from '@mui/icons-material/LocalActivity';
+import PaidIcon from '@mui/icons-material/Paid';
+import {
+	useNavigate 
+} from 'react-router-dom';
+
+const iconStyles = {
+	fontSize: 50,
+	mx: 2
+};
+const buttons = [
+	{
+		label: 'Ticket Kopen',
+		icon: <LocalActivityIcon sx={iconStyles} />,
+		path: '/tickets'
+	},
+	{
+		label: 'Doneren',
+		icon: <PaidIcon sx={iconStyles} />,
+		path: '/donate'
+	},
+];
+export function ActionButtons(): JSX.Element {
+	const navigate = useNavigate();
+	const goToPage = React.useCallback((location:string) : void => {
+		navigate(location);	
+	},[navigate]);
+	return (
+		<Grid container spacing={3}
+			sx={{
+				mb: 8
+			}}>
+			{
+				buttons.map((data,index) => (
+					<Grid item xs={12}
+						sm={6}
+						key={data.label}
+					>
+						<Slide in
+							timeout={325}
+							direction={index % 2 ? 'left' : 'right'}>
+							<Card elevation={24} sx={{
+								bgcolor:'primary.main'
+							}}>
+								<CardActionArea
+									onClick={() : void => goToPage(data.path)}
+									sx={{
+										p: 1,
+										display: 'flex',
+									}}>
+									{data.icon}
+									<Typography variant='h3' sx={{
+										fontWeight: 'bold'
+									}}>
+										{data.label}
+									</Typography>
+								</CardActionArea>
+
+							</Card>
+						</Slide>
+					</Grid>
+				)
+				)
+			}
+		</Grid>
+	);
+}
