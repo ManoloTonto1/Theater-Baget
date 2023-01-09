290fa5fbcd6e3b1ab2ed65716b6b29665b2026c9
waweewawee <wilmsakasha@gmail.com>
2023-01-09 10:47:19 +0100

    search bar verwijderd voor nu

--- client/src/routes/admin/AdminPage.tsx
+++ client/src/routes/admin/AdminPage.tsx
@@ -22,48 +22,6 @@ import {
 import UserContext from '../../context/UserContext';
 import img from '../../assets/poster.png';
 
-const Search = styled('div')(({ theme }) => ({
-	position: 'relative',
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	'&:hover': {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: '100%',
-	[theme.breakpoints.up('sm')]: {
-		marginLeft: theme.spacing(1),
-		width: 'auto',
-	},
-}));
-  
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: '100%',
-	position: 'absolute',
-	pointerEvents: 'none',
-	display: 'flex',
-	alignItems: 'center',
-	justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: 'inherit',
-	'& .MuiInputBase-input': {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create('width'),
-		width: '100%',
-		[theme.breakpoints.up('sm')]: {
-			width: '12ch',
-			'&:focus': {
-				width: '20ch',
-			},
-		},
-	},
-}));
-
 function ErrorPage() : JSX.Element{
 	const { theme } = React.useContext(UserContext);
 	const navigate = useNavigate();
@@ -119,17 +77,6 @@ function ErrorPage() : JSX.Element{
 							<Typography>Gebruiker inzien</Typography>
 							<TextField id="standard-basic" label="Naam"
 								variant="standard" />
-							<Search>
-								<SearchIconWrapper>
-									<SearchIcon />
-								</SearchIconWrapper>
-								<StyledInputBase
-									placeholder="Search…"
-									inputProps={{
-										'aria-label': 'search' 
-									}}
-								/>
-							</Search>
 						</Card>
 					</Grid>
 				</Container>
