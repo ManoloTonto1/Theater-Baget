98a7371b4d09c7c823889add2e2e4f1b8c9d70f6
GeenDrive <66233204+GeneDrive@users.noreply.github.com>
2023-01-13 14:15:22 +0100

    Profiel fixes  (#86)
    * started fixing profile
    
    * profiel fixes done

--- client/src/routes/profiel/tickets/ProfielTickets.tsx
+++ client/src/routes/profiel/tickets/ProfielTickets.tsx
@@ -1,18 +1,77 @@
-import { Box } from '@mui/material';
+import {
+	Box, Card, CardContent, Container, Grid, TextField, Typography 
+} from '@mui/material';
+import {
+	LocalizationProvider, StaticDatePicker 
+} from '@mui/x-date-pickers';
+import {
+	AdapterDayjs 
+} from '@mui/x-date-pickers/AdapterDayjs';
+import type {
+	Dayjs 
+} from 'dayjs';
+import dayjs from 'dayjs';
 import React from 'react';
-import { getUserTickets } from '../../../tools/TicketTools';
+import type {
+	TicketsProps
+} from '../../../components/Ticket';
+import {
+	Ticket 
+} from '../../../components/Ticket';
+import Monki from '../../../assets/gorilla.jfif';
+import Baget from '../../../assets/baguette.png';
+import Post from '../../../assets/poster.png';
 
 function ProfielTickets() {
+	const [value, setValue] = React.useState<Dayjs | null>(dayjs());
+	const [data, setData] = React.useState<never[] | TicketsProps[]>([]);
+	React.useEffect(() => {
+		// API('programmeringen').Get(value)
+		// 	.then((res) => {
+		// 		if (res.status != 200) {
+		// 			return;
+		// 		}
+
+		// 	});
+		setData([{
+			date: 'JAN 08 2023',
+			time: 'zo - 19:30',
+			name: 'Monki in paris',
+			location: '013 - Tilburg',
+			image: Monki
+		},
+		{
+			date: 'JAN 08 2023',
+			time: 'zo - 19:30',
+			name: 'Baget',
+			location: '013 - Tilburg',
+			image: Baget
+		},
+		{
+			date: 'JAN 08 2023',
+			time: 'zo - 19:30',
+			name: 'Baget',
+			location: '013 - Tilburg',
+			image: Post
+		}
+		]);
+	}, [value]);
+
 	return (
 		<Box sx={{
-            alignContent: 'center',
-			width: 700,
-			overflowY: 'scroll',
-        }}>
-			{getUserTickets()}
-			{getUserTickets()}
-			{getUserTickets()}
+			p: 1,
+			scrollbarWidth: 'thin',
+		}}>
+			<CardContent>
+				<Typography variant='h4' mb={2}>
+					Tickets:
+				</Typography>
+				{data.map((card) => {
+					return <Ticket key={card.name} {...card} />;
+				})}
+			</CardContent>
 		</Box>
+
 	);
 }
 
