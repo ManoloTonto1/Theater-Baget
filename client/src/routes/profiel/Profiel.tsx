554171a906ca91c09bda360867b84df8dee0c6a2
GeenDrive <66233204+GeneDrive@users.noreply.github.com>
2023-01-13 12:06:07 +0100

    User overzicht pagina (#84)
    * edited ticket tools for later use
    
    * started on profile page and logout
    
    * tickets, settings and logout pages front-end done
    
    morgen komt de info deel wel, zelfde met de band en bandleden pagina
    
    * profiel overzicht basic frontend done

--- client/src/routes/profiel/Profiel.tsx
+++ client/src/routes/profiel/Profiel.tsx
@@ -1,8 +1,134 @@
+import { Box, Card, Container, Grid, Tab, Tabs, Typography } from '@mui/material';
+import { max } from 'cypress/types/lodash';
 import React from 'react';
+import ProfielSettings from './settings/ProfielSettings';
+import ProfielTickets from './tickets/ProfielTickets';
+import Logout from './logout/Logout';
+import Monki from '../../assets/gorilla.jfif';
+import Avatar from '@mui/material/Avatar';
 
-function Profiel() {
+interface TabPanelProps {
+	children?: React.ReactNode;
+	index: number;
+	value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+	const { children, value, index, ...other } = props;
+  
 	return (
-		<div>Profiel</div>
+	  <div
+		role="tabpanel"
+		hidden={value !== index}
+		id={`vertical-tabpanel-${index}`}
+		aria-labelledby={`vertical-tab-${index}`}
+		{...other}
+	  >
+		{value === index && (
+		  <Box sx={{ p: 3 }}>
+			<Typography>{children}</Typography>
+		  </Box>
+		)}
+	  </div>
+	);
+}
+
+function a11yProps(index: number) {
+	return {
+		id: `vertical-tab-${index}`,
+		'aria-controls': `vertical-tabpanel-${index}`,
+	};
+}
+
+// source for tabs: https://mui.com/material-ui/react-tabs/ en de login page
+function Profiel() {
+	const [value, setValue] = React.useState(0);
+  
+	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+	  setValue(newValue);
+	};
+
+	return (
+		<Container style={{
+            height: '80vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        }}>
+			<Card elevation={4} sx={{ 
+				height: 485,
+				width: 500,
+				p: 1,
+				marginRight: 4,
+			}}>
+				<Card elevation={4} sx={{
+					marginBottom: 2,
+					display: 'flex',
+					justifyContent: 'center'
+				}}>
+					<Grid container>
+						<Avatar
+							alt="Remy Sharp"
+							src={Monki}
+							sx={{
+								width: 100,
+								height: 100,
+								m: 2
+							}} />
+						<Box sx={{
+							display: 'flex',
+							justifyContent: 'center',
+							flexDirection:'column'
+						}}>
+							<Typography>
+								Naam: akasha
+							</Typography>
+							<Typography>
+								Email: pidip@furrie.ninja
+							</Typography>
+							<Typography>
+								Leeftijd: dood
+							</Typography>
+						</Box>
+					</Grid>
+					
+				</Card>
+				<Tabs
+					orientation="vertical"
+					variant="fullWidth"
+					value={value}
+					onChange={handleChange}
+					aria-label="Vertical tabs"
+					sx={{ 
+						borderRight: 1, 
+						borderColor: 'divider' 
+					}}>
+					<Tab label="Tickets" {...a11yProps(0)} />
+					<Tab label="Settings" {...a11yProps(1)} />
+					<Tab label="Log out" {...a11yProps(2)} />
+				</Tabs>
+			</Card>
+
+			<Card elevation={4} sx={{ 
+				height: 485,
+				width: "fit-content",
+				minWidth: 500,
+				p: 1,
+			}}>
+				<TabPanel value={value} index={0}>
+					<ProfielTickets />
+				</TabPanel>
+				<TabPanel value={value} index={1}>
+					<ProfielSettings />
+				</TabPanel>
+				<TabPanel value={value} index={2}>
+					<Logout />
+				</TabPanel>
+			</Card>
+
+		</Container>
+
+
 	);
 }
 
--- client/src/routes/profiel/settings/ProfielSettings.tsx
+++ client/src/routes/profiel/settings/ProfielSettings.tsx
@@ -1,8 +1,94 @@
-import React from 'react';
+import { Box, Button, FormGroup, TextField, Typography } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import React, { useCallback } from 'react';
 
 function ProfielSettings() {
+	const [naam, setNaam] = React.useState('');
+	const [confirmPassword, setConfirmPassword] = React.useState('');
+	const [geboorteDatum, setGeboorteDatum] = React.useState<Dayjs | null>(dayjs());
+	const [password, setPassword] = React.useState('');
+	const [email, setEmail] = React.useState('');
+	const [passwordsMatch, setPasswordsMatch] = React.useState(true);
+
+	const handlePassword = useCallback(async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setPassword(e.target.value);
+	}, []);
+
+	const handleEmail = useCallback(async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setEmail(e.target.value);
+	}, []);
+
+	const handleConfirmPassword = useCallback(async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setConfirmPassword(e.target.value);
+	}, []);
+
+	const handleNaam = useCallback(async (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setNaam(e.target.value);
+	}, []);
+
 	return (
-		<div>ProfielSettings</div>
+		<FormGroup>
+			<Typography variant="h5" sx={{ 
+				m: 1
+			}}>
+                Gegevens aanpassen
+            </Typography>
+			
+			<TextField sx={{ 
+				m: 1, mb: 2 
+			}} 
+			label='Naam' 
+			variant='standard' 
+			type='text' 
+			required 
+			onChange={handleNaam}
+			value={naam}
+			/>
+
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DatePicker
+					label="Geboorte datum"
+					value={geboorteDatum}
+					onChange={(newValue) => {
+						setGeboorteDatum(newValue);
+					}}
+					renderInput={(params) => <TextField sx={{
+						m:1
+					}} variant='standard'
+					{...params} />}
+				/>
+			</LocalizationProvider>
+
+			<TextField sx={{
+				m: 1, mb: 2 
+			}} label='E-mail adres'
+			variant='standard' type='email'
+			required onChange={handleEmail} 
+			value={email}/>
+
+			<TextField sx={{
+				m: 1, mb: 2 
+			}} label='Wachtwoord'
+			variant='standard' type='password'
+			required onChange={handlePassword} 
+			value={password}/>
+
+			<TextField sx={{
+				m: 1, mb: 3
+			}} label='Wachtwoord bevestigen'
+			variant='standard' type='password'
+			required onChange={handleConfirmPassword}
+			value={confirmPassword}
+			error={!passwordsMatch} 
+			helperText={!passwordsMatch ? 'Wachtwoorden komen niet overeen' : ''}
+			/>
+
+			<Button variant='contained' type='submit'>
+				wijzigingen opslaan
+			</Button>
+		</FormGroup>
 	);
 }
 
