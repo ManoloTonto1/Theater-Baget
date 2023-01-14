9df4321a8ba9cab4d2f31eef3fe92fdfb9b29c6a
Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
2023-01-15 00:42:18 +0100

    changes to the cors thing

--- server/Program.cs
+++ server/Program.cs
@@ -8,6 +8,15 @@ using server.Controllers;
 
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "Client",
+                      policy =>
+                      {
+                          policy.WithOrigins("https://manolotonto1.github.io/",
+                          "https://manolotonto1.github.io/Theater-Baget/");
+                      });
+});
 builder.Services.AddDbContext<theaterContext>(options =>
     options.UseSqlite("Data Source=database.db"));
 // Add services to the container.
@@ -26,8 +35,8 @@ if (app.Environment.IsDevelopment())
 // enable if you want t use HTTPS, remember to change the Proxy in the client.
 // app.UseHttpsRedirection();
 
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
 
 JsonSerializerOptions options = new()
