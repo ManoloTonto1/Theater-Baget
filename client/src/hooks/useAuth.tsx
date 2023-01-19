5162ed228a8722076eaa1ea97d7c85badda2eaef
pidip47 <mitchell2612@gmail.com>
2023-01-19 11:44:07 +0100

    Squashed commit of the following:
    commit e95be7f6cb0b5f09012625a1f9462136bd890dd6
    Author: Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
    Date:   Wed Jan 18 20:57:19 2023 +0100
    
        Token (#123)
    
        * Finished the login and the token error
    
        * removed logs
    
        * weird shit with useAuth not working
    
    commit 945bd310ee9b4e9ef04cc0b56264b5c5f98c9a69
    Author: Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
    Date:   Wed Jan 18 14:40:25 2023 +0100
    
        Api fix (#119)
    
        * changes to the api and the jwt token
    
        * ADvancements with the apis
    
        * Finished the features but no tests because IDK how to test them
    
        * added an extra parameter to the api class in the client
    
        * fixed server errors
    
        * tests, are broken no time to fix to fuck it.
    
        * removed conflicts
    
        * Martijn is very happy
    
    commit dfc139412df44ebd1f143e42b60d7c47e68f394e
    Author: pidip47 <60977549+pidip47@users.noreply.github.com>
    Date:   Wed Jan 18 04:52:42 2023 -0800
    
        gebruiker inzien (#118)
    
    commit d244b1ad0dd4db6b9c8826f5e879e1fdd42756c8
    Author: Manuel Lopez <74105210+ManoloTonto1@users.noreply.github.com>
    Date:   Wed Jan 18 10:30:15 2023 +0100
    
        Api fix (#117)
    
        * changes to the api and the jwt token
    
        * ADvancements with the apis
    
        * Finished the features but no tests because IDK how to test them
    
        * added an extra parameter to the api class in the client
    
        * fixed server errors
    
        * tests, are broken no time to fix to fuck it.
    
        * removed conflicts

---
+++ client/src/hooks/useAuth.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+	useLocation
+} from 'react-router-dom';
+import API from '../api/apiRoutes';
+import UserContext from '../context/UserContext';
+
+function useAuth() {
+	const { user } = React.useContext(UserContext);
+
+	const [isValid, setValid] = React.useState(false);
+	const location = useLocation();
+	React.useEffect(() => {
+		if (!localStorage.getItem('token')) {
+			setValid(false);
+		}
+		if (!user.userData) {
+			API('validate').Get('user')
+				.then((res) => {
+					if (res.status != 200) {
+						localStorage.removeItem('token');
+						user.setUser(null);
+						return;
+					}
+					setValid(true);
+					user.setUser({
+						email: res.data.gebruiker.loginGegevens.email,
+						id: res.data.gebruiker.id,
+						naam: res.data.gebruiker.naam,
+						token: localStorage.getItem('token') as string
+						
+					});
+	
+				}).catch(() => {
+					localStorage.removeItem('token');
+					user.setUser(null);
+				});	
+			return;
+		}
+		API('validate').Get(undefined)
+			.then((res) => {
+				if (res.status != 200) {
+					localStorage.removeItem('token');
+					user.setUser(null);
+					return;
+				}
+				setValid(true);
+
+			}).catch(() => {
+				localStorage.removeItem('token');
+				user.setUser(null);
+			});
+	}, [location]);
+	
+	return isValid;
+}
+
+export default useAuth;
