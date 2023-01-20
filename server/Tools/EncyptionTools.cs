605a7c58d3604a34b44701d2946d8ed31cd1444b
pidip47 <mitchell2612@gmail.com>
2023-01-20 11:57:40 +0100

    password hashing

--- server/Tools/EncyptionTools.cs
+++ server/Tools/EncyptionTools.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography;
 using System.Text;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
 public class EncryptionTools {
 
     public static string GenerateKey(int userId) {
@@ -128,5 +130,19 @@ public class EncryptionTools {
 			}  
 			return builder.ToString();  
 		}  
+    }
+    public static string hashPassword(string password)
+    {
+	    byte[] salt = Encoding.ASCII.GetBytes("dikke_boktor");
+		
+        // derive a 256-bit subkey (use HMACSHA256 with 100,000 iterations)
+        string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+        return hashed;
     }
 }
